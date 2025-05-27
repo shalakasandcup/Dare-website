af70602558720589a31a4b2ef8d4aac179e2efc6
Madhavi <madhavisandcup@gmail.com>
2025-05-27 15:49:59 +0530

    first commit changes

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dare - Mission and Vision</title>
+
+    <link rel='icon' type='image/png' sizes='16x16' href='./images/favicon.svg'>
+    <link href="css/typography.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/responsive.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+</body>
+
+</html>
